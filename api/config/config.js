cad034aba221f31a3234db5274234219ef82dd98
baa4ts <baa4tsdev@gmail.com>
2025-08-23 20:30:37 -0300

    Test 3

--- api/config/config.js
+++ api/config/config.js
@@ -1,20 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import setupSwagger from './swagger.js';
+import setupSwagger from '../swagger.js';
 
 dotenv.config();
 const ser = express();
 
 // Configuración
 ser.use(express.json());
-const PORT = process.env.PORT || 3000;
 
-// Swagger
+// Swagger (opcional)
 setupSwagger(ser);
-
-// Listener
-ser.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
 
 export { ser };
--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
-import { reporterRouter } from './apps/reportes/reportes.js';
-import { sensorRouter } from './apps/sensores/sensores.js';
-import { userRouter } from './apps/user/user.js';
-import { ser } from './config/config.js';
+import { ser } from '../config/config.js';
+import { userRouter } from '../apps/user/user.js';
+import { sensorRouter } from '../apps/sensores/sensores.js';
+import { reporterRouter } from '../apps/reportes/reportes.js';
 
+// Montamos las rutas
 ser.use('/user', userRouter);
 ser.use('/sensor', sensorRouter);
 ser.use('/reports', reporterRouter);
 
+// Exportamos como función serverless
 export default ser;
