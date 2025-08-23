c819f937cb8c064dd614199e7ed90ec9525cac62
baa4ts <baa4tsdev@gmail.com>
2025-08-23 20:43:43 -0300

    Test 8

--- api/index.js
+++ api/index.js
@@ -1,12 +1,8 @@
-import { ser } from '../config/config.js';
-import { userRouter } from '../apps/user/user.js';
-import { sensorRouter } from '../apps/sensores/sensores.js';
-import { reporterRouter } from '../apps/reportes/reportes.js';
+import { reporterRouter } from './apps/reportes/reportes.js';
+import { sensorRouter } from './apps/sensores/sensores.js';
+import { userRouter } from './apps/user/user.js';
+import { ser } from './config/config.js';
 
-// Montamos las rutas
 ser.use('/user', userRouter);
 ser.use('/sensor', sensorRouter);
 ser.use('/reports', reporterRouter);
-
-// Exportamos como función serverless
-export default ser;
